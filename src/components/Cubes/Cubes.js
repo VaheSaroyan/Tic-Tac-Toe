ba603b4efb8fe36bcec1da98510363d78e0108da
Vahe Saroyan <vahe.saroyan.web@gmail.com>
2020-11-02 20:02:22 +0400

    change icons performance

--- src/components/Cubes/Cubes.js
+++ src/components/Cubes/Cubes.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { Cube, Row } from "../";
 import { THEMES_WITHOUT_DEFAULT } from "../../constants";
+import style1X from "./assets/style-1/icons/X.svg";
+import style1O from "./assets/style-1/icons/O.svg";
+import style2X from "./assets/style-2/icons/X.svg";
+import style2O from "./assets/style-2/icons/O.svg";
+import style3X from "./assets/style-3/icons/X.svg";
+import style3O from "./assets/style-3/icons/O.svg";
+import style4X from "./assets/style-4/icons/X.svg";
+import style4O from "./assets/style-4/icons/O.svg";
+import style5X from "./assets/style-5/icons/X.svg";
+import style5O from "./assets/style-5/icons/O.svg";
+
+const styles = {
+  "style-1": { X: style1X, O: style1O },
+  "style-2": { X: style2X, O: style2O },
+  "style-3": { X: style3X, O: style3O },
+  "style-4": { X: style4X, O: style4O },
+  "style-5": { X: style5X, O: style5O },
+};
 
 const Cubes = ({ cube, setItem, winRow, rowPosition, context }) => {
   const { theme } = context;
@@ -9,16 +27,18 @@ const Cubes = ({ cube, setItem, winRow, rowPosition, context }) => {
   React.useEffect(() => {
     if (THEMES_WITHOUT_DEFAULT.includes(theme)) {
       setImage(true);
-      if (theme)
-        (async () => {
-          const [{ default: O }, { default: X }] = await Promise.all([
-            import(`./assets/${theme}/icons/O.svg`),
-            import(`./assets/${theme}/icons/X.svg`),
-          ]);
-          if (X && O) {
-            setImage({ X, O });
-          }
-        })();
+      if (theme) {
+        // (async () => {
+        // const [{ default: O }, { default: X }] = await Promise.all([
+        //   import(`./assets/${theme}/icons/O.svg`),
+        //   import(`./assets/${theme}/icons/X.svg`),
+        // ]);
+        const { X, O } = styles[theme];
+        if (X && O) {
+          setImage({ X, O });
+        }
+      }
+      // })();
     } else {
       setImage(false);
     }
